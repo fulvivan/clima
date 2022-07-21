57f03a81d30270abe100e6cb683f2dca4bc52749
Fulvia Mauriello <fulvivan@gmail.com>
2022-07-21 12:55:14 +0200

    app weather

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+
+
 window.addEventListener("load", () => {
   var lon;
   var lat;
 
-  var apiKey = "personal api key";
+    var apiKey = "30b10e22b839e9b3ea31070db4685ebe";
+    //
 
   var temperaturaValore = document.getElementById("temperatura-valore");
 
